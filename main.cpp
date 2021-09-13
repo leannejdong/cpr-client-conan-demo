1660f40113c2a688bc30bc1a14f3c3d126946827
Leanne J Dong <leanne@bitwyre.com>
2021-09-13 18:51:22 -0400

    add gitignore and a comment in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,20 +6,23 @@
 int main(int argc, char** argv) {
 
   //Option1: Asynchronous response : handle the response at some later time
-  std::vector<std::future<cpr::Response>> responses;
-  auto url = cpr::Url{"https://httpbin.org/get"};
-  for(int i{0}; i < 1000; ++i){
-    responses.emplace_back(cpr::GetAsync(url));
-  }
-  for(auto& r : responses){
-    std::cout << r.get().text << "\n";
-  }
-
-  //Option2: Asynchronous callbacks
-  auto future = cpr::GetCallback([](cpr::Response r){
-    return r.text;
-  }, cpr::Url{"https://httpbin.org/get"});
-  std::cout << future.get() << "\n";
+//  std::vector<std::future<cpr::Response>> responses;
+//  auto url = cpr::Url{"https://httpbin.org/get"};
+//  for(int i{0}; i < 1000; ++i){
+//    responses.emplace_back(cpr::GetAsync(url));
+//  }
+//  for(auto& r : responses){
+//    std::cout << r.get().text << "\n";
+//  }
+//
+  //Option2: Asynchronous callbacks. Call some custom functions when we get the response
+//  auto future = cpr::GetCallback([](cpr::Response r){
+//    return r.text;
+//  }, cpr::Url{"https://httpbin.org/get"});
+//  std::cout << future.get() << "\n";
+   cpr::Response r = cpr::Get(cpr::Url{"http://www.httpbin.org/get"});
+   std::cout << r.url << std::endl; // http://www.httpbin.org/get?hello=world
 
 }
+
 
